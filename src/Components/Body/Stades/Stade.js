07875a55be26d7c81336f8e63150d04526981114
Oussama Fekih Hassen <hssanoussama@gmail.com>
2024-04-24 14:23:53 +0200

    update stade

--- src/Components/Body/Stades/Stade.js
+++ src/Components/Body/Stades/Stade.js
@@ -79,7 +79,6 @@ const Stade = ({ scrollY, threshold }) => {
             width={"100%"}
           />
         ))}
-<<<<<<< Updated upstream
         <div className="second" >
           <Button rightIcon={<AddIcon />} colorScheme='blue' onClick={onOpen} marginLeft={'45%'} marginTop={'30px'}>
             Créer une demande
@@ -161,83 +160,3 @@ const Stade = ({ scrollY, threshold }) => {
   );
 };
 export default Stade;
-=======
-        
-      </div>
-      {/* <div className="carousel-buttons">
-        <button className="button-arrow" onClick={() => updateIndex(activeIndex - 1)}>
-          <ArrowLeftIcon />
-        </button>
-        <button className="button-arrow" onClick={() => updateIndex(activeIndex + 1)}>
-          <ArrowRightIcon />
-        </button>
-      </div> */}
-      
-       
-        <Drawer
-          isOpen={isOpen}
-          placement='right'
-          initialFocusRef={firstField}
-          onClose={onClose}
-        >
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader borderBottomWidth='1px'>
-              Créer une nouvelle demande
-            </DrawerHeader>
-  
-            <DrawerBody>
-              <Stack spacing='24px'>
-                <Box>
-                  <FormLabel htmlFor='username'>Nom Complet</FormLabel>
-                  <Input
-                    ref={firstField}
-                    id='username'
-                    placeholder='Please enter user name'
-                  />
-                </Box>
-  
-                <Box>
-                  <FormLabel htmlFor='url'>Url</FormLabel>
-                  <InputGroup>
-                    <InputLeftAddon>http://</InputLeftAddon>
-                    <Input
-                      type='url'
-                      id='url'
-                      placeholder='Please enter domain'
-                    />
-                    <InputRightAddon>.com</InputRightAddon>
-                  </InputGroup>
-                </Box>
-  
-                <Box>
-                  <FormLabel htmlFor='owner'>Select Owner</FormLabel>
-                  <Select id='owner' defaultValue='segun'>
-                    <option value='segun'>Segun Adebayo</option>
-                    <option value='kola'>Kola Tioluwani</option>
-                  </Select>
-                </Box>
-  
-                <Box>
-                  <FormLabel htmlFor='desc'>Description</FormLabel>
-                  <Textarea id='desc' />
-                </Box>
-              </Stack>
-            </DrawerBody>
-  
-            <DrawerFooter borderTopWidth='1px'>
-              <Button variant='outline' mr={3} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button colorScheme='blue'>Submit</Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </Drawer>
-        
-    </div>
-  );
-};
-
-export default Stade;
->>>>>>> Stashed changes
