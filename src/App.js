fbc3d7ffef7aadeefd92f87fe882e0ff451beb9d
Oussama Fekih Hassen <hssanoussama@gmail.com>
2024-04-24 20:52:36 +0200

    fix chakra

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import Navbar from './Components/Navbar/Navbar';
 import Body from './Components/Body/Body';
 import Footer from './Components/Footer/Footer';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="app-container">
-      <Navbar />
-      <div className="body-container">
-        <Body />
+    <ChakraProvider>
+      <div className="app-container">
+        <Navbar />
+        <div className="body-container">
+          <Body />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ChakraProvider>
+    
   );
 }
 
